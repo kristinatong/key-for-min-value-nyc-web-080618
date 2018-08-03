2b4a0990989a43381a20330aa3de43428ea0c97a
Kristina Tong <kristina.tong@gmail.com>
2018-08-03 22:35:31 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
   if name_hash > 0
-    min_key = name_hash.keys[0]
+    min_key = name_hash.each{|k,v| k}[0]
     min_val = name_hash.values[0]
     name_hash.each do |key,value|
       if value < min_val
