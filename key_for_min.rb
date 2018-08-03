cd2c73b0a8f7953c719796866470ef1cf1e98260
Kristina Tong <kristina.tong@gmail.com>
2018-08-03 21:52:52 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,5 +3,10 @@
 
 def key_for_min_value(name_hash)
   name_hash.collect do |key,value|
-    
+    name_hash.collect do |key2,value2|
+      if value < value2
+        min = value
+      end
+    end
+  end
 end
